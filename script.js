3d122539dbc3952424134fb3a0e4c0322e8c504a
Jerome <dreammedia365@yahoo.com>
2019-10-27 20:53:45 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,121 @@
+$("#play-video1").on("click", function() {
+  $(".bone")
+    .get(0)
+    .pause();
+});
+
+$("#play-video2").on("click", function() {
+  $(".bone2")
+    .get(0)
+    .pause();
+});
+
+$("#play-video3").on("click", function() {
+  $(".bone3")
+    .get(0)
+    .pause();
+});
+
+$("#play-video4").on("click", function() {
+  $(".bone4")
+    .get(0)
+    .pause();
+});
+
+$("#play-video5").on("click", function() {
+  $(".bone5")
+    .get(0)
+    .pause();
+});
+
+$("#play-video6").on("click", function() {
+  $(".bone6")
+    .get(0)
+    .pause();
+});
+
+$("#play-video7").on("click", function() {
+  $(".bone7")
+    .get(0)
+    .pause();
+});
+
+$("#play-video8").on("click", function() {
+  $(".bone8")
+    .get(0)
+    .pause();
+});
+
+$("#play-video9").on("click", function() {
+  $(".bone9")
+    .get(0)
+    .pause();
+});
+
+$("#play-video10").on("click", function() {
+  $(".bone10")
+    .get(0)
+    .pause();
+});
+
+$("#play-video11").on("click", function() {
+  $(".bone11")
+    .get(0)
+    .pause();
+});
+
+$("#play-video12").on("click", function() {
+  $(".bone12")
+    .get(0)
+    .pause();
+});
+
+$("#play-video13").on("click", function() {
+  $(".bone13")
+    .get(0)
+    .pause();
+});
+
+$("#play-video14").on("click", function() {
+  $(".bone14")
+    .get(0)
+    .pause();
+});
+
+$("#play-video15").on("click", function() {
+  $(".bone15")
+    .get(0)
+    .pause();
+});
+
+$("#play-video16").on("click", function() {
+  $(".bone16")
+    .get(0)
+    .pause();
+});
+
+$("#play-video17").on("click", function() {
+  $(".bone17")
+    .get(0)
+    .pause();
+});
+
+$("#play-video18").on("click", function() {
+  $(".bone18")
+    .get(0)
+    .pause();
+});
+
+$("#play-video19").on("click", function() {
+  $(".bone19")
+    .get(0)
+    .pause();
+});
+
+$("#play-video20").on("click", function() {
+  $(".bone20")
+    .get(0)
+    .pause();
+});
+
+
